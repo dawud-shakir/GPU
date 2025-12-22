01d04ca849bea8626ebe3aa2e8b5cad4795e4bed
dawud-shakir <dsshakir@gmail.com>
2025-12-22 15:44:12 -0700

    Added run script

---
+++ Nvidia Website/Cuda Programming Guide/Part 2/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+vectorLength="${1:-1024}"
+
+nvcc -O3 -gencode arch=compute_75,code=sm_75 unified_memory.cu -o unified_memory
+nvcc -O3 -gencode arch=compute_75,code=sm_75 explicit_memory.cu -o explicit_memory
+
+./unified_memory vectorLength
+./explicit_memory vectorLength
