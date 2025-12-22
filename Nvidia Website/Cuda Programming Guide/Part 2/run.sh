1019580af8e89497213441090605c35db0dd8b56
Dawud <dsshakir@gmail.com>
2025-12-22 23:24:36 +0000

    run.sh

--- Nvidia Website/Cuda Programming Guide/Part 2/run.sh
+++ Nvidia Website/Cuda Programming Guide/Part 2/run.sh
@@ -4,7 +4,9 @@
 vectorLength="${1:-1024}"
 
 nvcc -O3 -gencode arch=compute_75,code=sm_75 unified_memory.cu -o unified_memory
-nvcc -O3 -gencode arch=compute_75,code=sm_75 explicit_memory.cu -o explicit_memory
 
 ./unified_memory "${vectorLength}"
+
+nvcc -O3 -gencode arch=compute_75,code=sm_75 explicit_memory.cu -o explicit_memory
+
 ./explicit_memory "${vectorLength}"
