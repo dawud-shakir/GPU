5c1bd089f4bdd1bb814fd52d80b05e2b728dc7f3
dawud-shakir <dsshakir@gmail.com>
2025-12-22 15:51:12 -0700

    Arguments

--- Nvidia Website/Cuda Programming Guide/Part 2/unified_memory.cu
+++ Nvidia Website/Cuda Programming Guide/Part 2/unified_memory.cu
@@ -172,7 +172,7 @@ void unifiedMemExample(int vectorLength)
 
 int main(int argc, char** argv)
 {
-    printf("argc=%d\n", argc)
+    printf("argc=%d\n", argc);
     int vectorLength = 1024;
     if(argc >=2)
     {
