e74f8bcfcfaf94f735700dcbbfb287fcff117629
dawud-shakir <dsshakir@gmail.com>
2025-12-31 06:09:18 -0700

    Added blas/transpose

--- blas & lapack/transpose_via_blas.c
+++ blas & lapack/transpose_via_blas.c
@@ -5,21 +5,29 @@
 
 int main() {
   const int m = 2, n = 3;            // A is m x n, AT is n x m
-  double A[m*n] = { 1,2,3, 4,5,6 };  // row-major: [ [1 2 3], [4 5 6] ]
-  double *I = calloc((size_t)m*m, sizeof(double));
+  double A[] = { 1, 2, 3, 4, 5, 6 }; // row-major: [ [1 2 3], [4 5 6] ]
+  double *Id = calloc((size_t)m * (size_t)m, sizeof(double));
   double *AT = calloc((size_t)n*m, sizeof(double));
 
-  for (int i = 0; i < m; ++i) I[i*m + i] = 1.0;
+  if (!Id || !AT) {
+    fprintf(stderr, "calloc failed\n");
+    free(Id);
+    free(AT);
+    return 1;
+  }
+
+  for (int i = 0; i < m; ++i) Id[i*m + i] = 1.0;
 
   // AT (n x m) = A^T (n x m) * I (m x m)
   cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans,
-              n, m, m, 1.0, A, n, I, m, 0.0, AT, m);
+              n, m, m, 1.0, A, n, Id, m, 0.0, AT, m);
 
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) printf("%g ", AT[i*m + j]);
     printf("\n");
   }
 
-  free(I); free(AT);
+  free(Id);
+  free(AT);
   return 0;
 }
