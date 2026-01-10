6a71763adb74576f7e1c71f224d5a903f261e716
dawud-shakir <dsshakir@gmail.com>
2026-01-10 08:47:45 -0700

    Added matrix multiplication from PMPP book

--- Resources/Programming Massively Parallel Processors/matmul.cu
+++ Resources/Programming Massively Parallel Processors/matmul.cu
@@ -73,8 +73,8 @@ int main(int argc, char* argv[])
     for (int row = 0; row < Width; ++row) {
         for (int col = 0; col < Width; ++col) {
             const int index = row * Width + col;
-            M[index] = row * Width + col;
-            N[index] = row * Width + col;
+            M[index] = 1; //row * Width + col;
+            N[index] = 1; //row * Width + col;
             P_gpu[index] = 0.0f;
             P_cpu[index] = 0.0f;
 
