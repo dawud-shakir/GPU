cb01e7b8792037ad69d5a891bb33356093b3bb87
dawud-shakir <dsshakir@gmail.com>
2026-01-10 08:47:45 -0700

    Added matrix multiplication from PMPP book

--- Resources/Programming Massively Parallel Processors/matmul.cu
+++ Resources/Programming Massively Parallel Processors/matmul.cu
@@ -1,13 +1,10 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-/**************
-For Ch. 3, Exercise 1 in PMPP book
-****************/
-
 /*****
-Con: Underutilizes threads
-***/
+ Ch. 3, Exercise 1.a
+*****/
+
 __global__
 void MatrixMul_RowsKernel(float* M, float* N,
                      float* P, int Width) {
@@ -29,7 +26,80 @@ void MatrixMul_RowsKernel(float* M, float* N,
     }
 }
 
+void MatrixMul_Rows(float* M, float* N,
+               float* P, int Width) {
+    int size = Width * Width * sizeof(float);
 
+    float *M_d, *N_d, *P_d;
+    cudaMalloc((void **)&M_d, size);
+    cudaMalloc((void **)&N_d, size);
+    cudaMalloc((void **)&P_d, size);
+
+    cudaMemcpy(M_d, M, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(N_d, N, size, cudaMemcpyHostToDevice);
+    
+    dim3 blockDim(1, 32);
+    dim3 gridDim(1, ceil(Width / (float)blockDim.y));
+
+    printf("blockDim: (%d, %d, %d)\n", blockDim.x, blockDim.y, blockDim.z);
+    printf("gridDim: (%d, %d, %d)\n", gridDim.x, gridDim.y, gridDim.z);
+
+    MatrixMul_RowsKernel<<<gridDim, blockDim>>>(M_d, N_d, P_d, Width);
+
+    cudaMemcpy(P, P_d, size, cudaMemcpyDeviceToHost);
+}
+
+/*****
+ Ch. 3, Exercise 1.b
+*****/
+
+__global__
+void MatrixMul_ColsKernel(float* M, float* N,
+                     float* P, int Width) {
+
+    
+    int row = blockIdx.y*blockDim.y + threadIdx.y;
+    int col = blockIdx.x*blockDim.x + threadIdx.x;
+
+    if (row < Width && col < Width) {
+        
+        for (int i = 0; i < Width; ++i) {
+            float Pvalue = 0;
+            for (int k = 0; k < Width; ++k) {
+                Pvalue += M[i*Width + k] * N[k*Width + col];
+            }
+
+            P[i*Width + col] = Pvalue;
+        }
+    }
+}
+
+void MatrixMul_Cols(float* M, float* N,
+               float* P, int Width) {
+    int size = Width * Width * sizeof(float);
+
+    float *M_d, *N_d, *P_d;
+    cudaMalloc((void **)&M_d, size);
+    cudaMalloc((void **)&N_d, size);
+    cudaMalloc((void **)&P_d, size);
+
+    cudaMemcpy(M_d, M, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(N_d, N, size, cudaMemcpyHostToDevice);
+    
+    dim3 blockDim(32, 1);
+    dim3 gridDim(ceil(Width / (float)blockDim.x), 1);
+
+    printf("blockDim: (%d, %d, %d)\n", blockDim.x, blockDim.y, blockDim.z);
+    printf("gridDim: (%d, %d, %d)\n", gridDim.x, gridDim.y, gridDim.z);
+
+    MatrixMul_RowsKernel<<<gridDim, blockDim>>>(M_d, N_d, P_d, Width);
+
+    cudaMemcpy(P, P_d, size, cudaMemcpyDeviceToHost);
+}
+
+/*****
+ Ch. 3, Lesson
+*****/
 
 __global__
 void MatrixMulKernel(float* M, float* N,
@@ -72,30 +142,6 @@ void MatrixMul(float* M, float* N,
     cudaMemcpy(P, P_d, size, cudaMemcpyDeviceToHost);
 }
 
-void MatrixMul_Rows(float* M, float* N,
-               float* P, int Width) {
-    int size = Width * Width * sizeof(float);
-
-    float *M_d, *N_d, *P_d;
-    cudaMalloc((void **)&M_d, size);
-    cudaMalloc((void **)&N_d, size);
-    cudaMalloc((void **)&P_d, size);
-
-    cudaMemcpy(M_d, M, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(N_d, N, size, cudaMemcpyHostToDevice);
-    
-    dim3 blockDim(1, 32);
-    dim3 gridDim(1, ceil(Width / (float)blockDim.y));
-
-    printf("blockDim: (%d, %d, %d)\n", blockDim.x, blockDim.y, blockDim.z);
-    printf("gridDim: (%d, %d, %d)\n", gridDim.x, gridDim.y, gridDim.z);
-
-    MatrixMul_RowsKernel<<<gridDim, blockDim>>>(M_d, N_d, P_d, Width);
-
-    cudaMemcpy(P, P_d, size, cudaMemcpyDeviceToHost);
-}
-
-
 void MatrixMulCPU(float* M, float* N,
                   float* P, int Width) {
     for (int row = 0; row < Width; ++row) {
@@ -137,7 +183,7 @@ int main(int argc, char* argv[])
         }
     }
 
-    MatrixMul_Rows(M, N, P_gpu, Width);
+    MatrixMul_Cols(M, N, P_gpu, Width);
     MatrixMulCPU(M, N, P_cpu, Width);
 
     bool match = true;
